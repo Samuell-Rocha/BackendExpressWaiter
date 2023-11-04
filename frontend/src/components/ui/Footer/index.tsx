ff6d8383847f736bf0b9db6c14f6e4bdd3393644
Samuel Rocha <85241271+Samuell-Rocha@users.noreply.github.com>
2023-11-03 22:48:57 -0300

    Delete frontend directory

--- frontend/src/components/ui/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.scss";
-
-export function Footer() {
-  return (
-    <footer className={styles.footerContainer}>
-      <h1>Copyright © 2023 - Express Waiter | Todos os direitos reservados </h1>
-    </footer>
-  );
-}
